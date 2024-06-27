ea7a686728977e12016a1076a60484fee946250f
Ben Weibel <weibel.b@gmail.com>
2024-06-26 17:15:50 -0700

    Initial Commit

---
+++ inc/Editor/Component.php
@@ -0,0 +1,138 @@
+<?php
+/**
+ * WP_Rig\WP_Rig\Editor\Component class
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig\Editor;
+
+use WP_Rig\WP_Rig\Component_Interface;
+use function add_action;
+use function add_theme_support;
+
+/**
+ * Class for integrating with the block editor.
+ *
+ * @link https://wordpress.org/gutenberg/handbook/extensibility/theme-support/
+ */
+class Component implements Component_Interface {
+
+	/**
+	 * Gets the unique identifier for the theme component.
+	 *
+	 * @return string Component slug.
+	 */
+	public function get_slug(): string {
+		return 'editor';
+	}
+
+	/**
+	 * Adds the action and filter hooks to integrate with WordPress.
+	 */
+	public function initialize() {
+		add_action( 'after_setup_theme', array( $this, 'action_add_editor_support' ) );
+	}
+
+	/**
+	 * Adds support for various editor features.
+	 */
+	public function action_add_editor_support() {
+		// Add support for editor styles.
+		add_theme_support( 'editor-styles' );
+
+		// Add support for default block styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Add support for wide-aligned images.
+		add_theme_support( 'align-wide' );
+
+		/**
+		 * Add support for color palettes.
+		 *
+		 * To preserve color behavior across themes, use these naming conventions:
+		 * - Use primary and secondary color for main variations.
+		 * - Use `theme-[color-name]` naming standard for standard colors (red, blue, etc).
+		 * - Use `custom-[color-name]` for non-standard colors.
+		 *
+		 * Add the line below to disable the custom color picker in the editor.
+		 * add_theme_support( 'disable-custom-colors' );
+		 */
+		add_theme_support(
+			'editor-color-palette',
+			array(
+				array(
+					'name'  => __( 'Primary', 'wp-rig' ),
+					'slug'  => 'theme-primary',
+					'color' => '#48285c',
+				),
+				array(
+					'name'  => __( 'Secondary', 'wp-rig' ),
+					'slug'  => 'theme-secondary',
+					'color' => '#004f5f',
+				),
+				array(
+					'name'  => __( 'Gray', 'wp-rig' ),
+					'slug'  => 'theme-gray',
+					'color' => '#4d4d4d',
+				),
+				array(
+					'name'  => __( 'Cream', 'wp-rig' ),
+					'slug'  => 'theme-cream',
+					'color' => '#fffbea',
+				),
+				array(
+					'name'  => __( 'Green', 'wp-rig' ),
+					'slug'  => 'theme-green',
+					'color' => '#3d752e',
+				),
+				array(
+					'name'  => __( 'Blue', 'wp-rig' ),
+					'slug'  => 'theme-blue',
+					'color' => '#004f5f',
+				),
+				array(
+					'name'  => __( 'Purple', 'wp-rig' ),
+					'slug'  => 'theme-purple',
+					'color' => '#48285c',
+				),
+			)
+		);
+
+		/*
+		 * Add support custom font sizes.
+		 *
+		 * Add the line below to disable the custom color picker in the editor.
+		 * add_theme_support( 'disable-custom-font-sizes' );
+		 */
+		add_theme_support(
+			'editor-font-sizes',
+			array(
+				array(
+					'name'      => __( 'Small', 'wp-rig' ),
+					'shortName' => __( 'S', 'wp-rig' ),
+					'size'      => 16,
+					'slug'      => 'small',
+				),
+				array(
+					'name'      => __( 'Medium', 'wp-rig' ),
+					'shortName' => __( 'M', 'wp-rig' ),
+					'size'      => 25,
+					'slug'      => 'medium',
+				),
+				array(
+					'name'      => __( 'Large', 'wp-rig' ),
+					'shortName' => __( 'L', 'wp-rig' ),
+					'size'      => 31,
+					'slug'      => 'large',
+				),
+				array(
+					'name'      => __( 'Larger', 'wp-rig' ),
+					'shortName' => __( 'XL', 'wp-rig' ),
+					'size'      => 39,
+					'slug'      => 'larger',
+				),
+			)
+		);
+	}
+}
