0010cebdbd31d5440837b28320d388cf8ea784cb
Ben Weibel <weibel.b@gmail.com>
2024-09-30 10:54:10 -0700

    Add cart animation

--- footer.php
+++ footer.php
@@ -12,12 +12,15 @@
 namespace WP_Rig\WP_Rig;
 
 ?>
-
+<div class="cart-wrap">
+	<div class="cart">
+			<img class="donkey" src="<?php echo get_theme_file_uri( '/assets/images/donkeycart.png' ); ?>" alt="" width="300px">
+			<img class="wheel" src="<?php echo get_theme_file_uri( '/assets/images/cartwheel.png' ); ?>" alt="" width="70px">
+		</div>
+</div>
 	<footer id="colophon" class="site-footer torn-top">
 		<?php get_template_part( 'template-parts/footer/info' ); ?>
-		<div class="donkey">
-			<img src="<?php echo get_theme_file_uri( '/assets/images/donkeycart.png' ); ?>" alt="" width="300px">
-		</div>
+		
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
